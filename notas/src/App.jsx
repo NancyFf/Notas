a3d60071a26b3b1916276bd2affca7e14b4aa75e
nancy farias fernandez <isc18350306@gmail.com>
2022-03-24 16:27:57 -0600

    input controlado

--- notas/src/App.jsx
+++ notas/src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+function App(){
+  //Todo: presentar el concepto de "state"//
+  let varprueba ="Titulo";
+  
+  const handleChangePrueba= (event) => { 
+  varprueba = event.target.value;
+  };
+  
   return (
-    <div className="App">
-     <h1>Hola</h1>
-    </div>
+  <div className="App">
+  <h3>NOtas</h3>
+  <label htmlFor="PruebaID">Input de prueba</label>
+  <input 
+  id="PruebaID" 
+  name="prueba" 
+  type="text"
+  anChange={handleChangePrueba}
+  value={varprueba}
+  />
+  </div>
   );
-}
-
-export default App;
+  }
+  export default App;
+  
