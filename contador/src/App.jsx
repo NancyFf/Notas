14846d1dc9a0b12b434f161031509db6db72d801
nancy farias fernandez <isc18350306@gmail.com>
2022-05-12 15:22:16 -0500

    agregacion de frase en la lista

--- contador/src/App.jsx
+++ contador/src/App.jsx
@@ -1,77 +1,83 @@
 import { useState } from "react";
 
-
 function App() {
-  const [cuenta, setcuenta ] = useState(0);
-  const [Paso, setPaso]= useState(1);
+  const [inputState, setInputState] = useState({
+    titulo: "",
+    fecha: "",
+    nota: "",
+    limpiar: "",
+  });
 
-  const handleClick = () => {
-    //console.log("clic");
-    setcuenta(cuenta + 1);
-  }
-  const handleClick1 = () => {
-    //console.log("clic");
-    setcuenta(0);
-    setPaso(1);
-  }
-  const handleClick2 = () => {
-    //console.log("clic");
-    setcuenta(cuenta - 1);
-  }
-  const handleClick3 = () => {
-    //console.log("clic");
-    setcuenta(cuenta - Paso);
-  }
-  const handleClick4 = () => {
-    //console.log("clic");
-    setcuenta(cuenta + Paso);
-  }
+  const handleResetClick = (v) =>{
+    setInputState({titulo: "",
+    fecha: "",
+    nota: "",
+    limpiar: "", });
+  };
+
+  const handleInputChange = (event) => {
+    //console.log(event.target);
+    setInputState({ ...inputState, [event.target.name]: event.target.value, });
+    console.log(event.target);
+  };
+
+  const handleClicklimpiar = () =>{
+    setInputState({title: "", date: "", note:""});
+  };
   
-  const handleInputChange = (e) => {
-    if(isNaN(e.target.value)){
-      return
-    }
-
-    setPaso(Number(e.target.value))
-  }
-
-
   return (
-    <div className="App">
-      <h3>CONTADOR</h3>
+  <div className="App container">
+    <div className="row">
+      <div className="col">
+        <h3> lista</h3> 
+      </div>
+      <div className="col">
+      <h3>Bienvenidos a mi Block de Notas</h3>
+      <label  className="mb-2">
+        Titulo
+      <input
+        id="titulo"
+        name="titulo"
+        type="text"
+        onChange={handleInputChange}
+        value={inputState.titulo}
+      />
+      </label >
       <br />
-      <hr/>
-      <h2 className="text-center">{cuenta}</h2>
+      <label  className="mb-2">
+        Fecha
+      <input
+          id="date"
+          name="date"
+          type="text"
+          onChange={handleInputChange}
+          value={inputState.date}
+        />
+      </label>
       <br />
-      <hr/>
-
-
-      <div style={{display:"flex", justifyContent:"flex-end", marginRight:"10px"}}>
-        <label htmlFor="">
-      Paso
-      <input id="paso" type="text" onChange={handleInputChange} style={{ marginLeft:"5px", width:"60px"}} />
-    </label>
-      </div>
-
-
-      <div style={{display:"flex", justifyContent:"flex-end", marginRight:"10px"}}>
-      <button type="button" className="btn btn-primary" style={{marginLeft:"5px"}}
-      onClick={handleClick}>+1</button>
-      <button type="button" className="btn btn-primary" style={{marginLeft:"5px"}}
-      onClick={handleClick1}>Resetear</button>
-      <button type="button" className="btn btn-primary" style={{marginLeft:"5px"}}
-      onClick={handleClick2}>-1</button>
-      </div>
-      <div style={{display:"flex", justifyContent:"flex-end", marginRight:"10px", marginTop:"10px"}}>
-      <button type="button" className="btn btn-primary" style={{marginLeft:"5px"}}
-      onClick={handleClick4}>+</button>
-      <button type="button" className="btn btn-primary" style={{marginLeft:"5px"}}
-      onClick={handleClick3}>-</button>
-      </div>
-      
-      
+      <label>
+        Nota
+        <input
+          id="note"
+          name="note"
+          type="text"
+          onChange={handleInputChange}
+          value={inputState.note}
+        />
+      </label>
+    </div>
+  </div>
+  <hr />
+  <div className="row">
+    <button
+      type="button"
+      className="btn btn-prymary" 
+      onClick={handleClicklimpiar}
+    >
+      limpiar
+      </button>
+     </div>
     </div>
   );
 }
-
 export default App;
